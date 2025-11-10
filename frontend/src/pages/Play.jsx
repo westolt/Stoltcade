cac60dce35e5ef265094565ec5d2aaae26cd6a89
westolt <william.stolt@helsinki.fi>
2025-11-10 21:05:34 +0200

    Fix game iframe display on Play page and fix index.html in PeriodicPairs

--- frontend/src/pages/Play.jsx
+++ frontend/src/pages/Play.jsx
@@ -8,10 +8,17 @@ const Play = ({ games }) => {
     if (!game) {
         return <h2>Game not found</h2>
     }
-
+    console.log('This is game url: ', game.url)
     return (
         <div className='playbox'>
             <h2>Now playing {game.name}</h2>
+            <iframe
+            src={game.url}
+            title={game.name}
+            width='800'
+            height='600'
+            style={{ border: 0}}
+            />
             <b>{game.description}</b>
         </div>
     )
