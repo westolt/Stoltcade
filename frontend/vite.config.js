85e76547958608d0c070c9956d64220ee6fb5807
westolt <westolt@ad.helsinki.fi>
2025-11-13 20:08:21 +0200

    Fix frontend dev environment by proxying /games to backend and fix play screen resoltion

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
@@ -10,6 +9,10 @@ export default defineConfig({
         target: 'http://localhost:3000',
         changeOrigin: true,
       },
-    }
+      '/games': {
+        target: 'http://localhost:3000',
+        changeOrigin: true,
+      },
+    },
   },
 })
