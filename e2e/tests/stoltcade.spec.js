5270aa631a81b94147e5b4616907b1c54b0ff823
westolt <william.stolt@helsinki.fi>
2025-11-24 20:42:07 +0200

    Add e2e test for opening a game and navigating from header to front page

--- e2e/tests/stoltcade.spec.js
+++ e2e/tests/stoltcade.spec.js
@@ -6,7 +6,7 @@ describe('Stoltcade', () => {
     })
 
     test('front page can be opened', async ({ page }) => {
-        await expect(page.getByRole('link', {name: 'Stoltcade' })).toBeVisible()
+        await expect(page.getByRole('link', { name: 'Stoltcade' })).toBeVisible()
         await expect(page.getByText('PeriodicPairs')).toBeVisible()
         await expect(page.getByText('Welcome to Stoltcade!')).toBeVisible()
     })
@@ -18,5 +18,30 @@ describe('Stoltcade', () => {
 
         const description = page.getByText('A quiz game about the periodic table.')
         await expect(description).toBeVisible({ timeout: 3000 })
+    })
+
+    test('game is opened when game button is clicked', async ({ page }) => {
+        const gameButton = page.getByRole('button', { name: 'PeriodicPairs' })
+        await expect(gameButton).toBeVisible()
+        await gameButton.click()
+
+        const how_to_play = page.getByText('Type the full name of the chemical element that matches the symbol shown on the screen and click Submit.')
+        await expect(how_to_play).toBeVisible({ timeout: 10000 })
+    })
+
+    test('navigate back to front page from play page after clicking Stoltcade header', async ({ page }) => {
+        const gameButton = page.getByRole('button', { name: 'PeriodicPairs' })
+        await expect(gameButton).toBeVisible()
+        await gameButton.click()
+
+        const how_to_play = page.getByText('Type the full name of the chemical element that matches the symbol shown on the screen and click Submit.')
+        await expect(how_to_play).toBeVisible({ timeout: 10000 })
+
+        const header = page.getByRole('link', { name: 'Stoltcade' })
+        await expect(header).toBeVisible()
+        await header.click()
+
+        const welcome = page.getByText('Welcome to Stoltcade!')
+        await expect(welcome).toBeVisible({ timeout: 10000 })
     })
 })
