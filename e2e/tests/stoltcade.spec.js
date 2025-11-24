d51d1b4f5fce8be9377bc81eb974b61cf24b1eae
westolt <william.stolt@helsinki.fi>
2025-11-24 20:10:23 +0200

    Add e2e test for game description to show on hover

--- e2e/tests/stoltcade.spec.js
+++ e2e/tests/stoltcade.spec.js
@@ -1,11 +1,22 @@
-const { test, describe, expect } = require('@playwright/test')
+const { test, describe, expect, beforeEach } = require('@playwright/test')
 
 describe('Stoltcade', () => {
-    test('front page can be opened', async ({ page }) => {
+    beforeEach(async ({ page }) => {
         await page.goto('http://localhost:5173')
+    })
 
+    test('front page can be opened', async ({ page }) => {
         await expect(page.getByRole('link', {name: 'Stoltcade' })).toBeVisible()
         await expect(page.getByText('PeriodicPairs')).toBeVisible()
         await expect(page.getByText('Welcome to Stoltcade!')).toBeVisible()
+    })
+
+    test('game description is shown when hovering cursor over a game button', async ({ page }) => {
+        const gameButton = page.getByRole('button', { name: 'PeriodicPairs' })
+        await expect(gameButton).toBeVisible()
+        await gameButton.hover()
+
+        const description = page.getByText('A quiz game about the periodic table.')
+        await expect(description).toBeVisible({ timeout: 3000 })
     })
 })
