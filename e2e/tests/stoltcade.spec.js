bfc849fc4ca563ed7a6749200d5f5821cfa74c73
westolt <william.stolt@helsinki.fi>
2025-11-24 21:58:13 +0200

    Add more time for e2e tests

--- e2e/tests/stoltcade.spec.js
+++ e2e/tests/stoltcade.spec.js
@@ -6,42 +6,42 @@ describe('Stoltcade', () => {
     })
 
     test('front page can be opened', async ({ page }) => {
-        await expect(page.getByRole('link', { name: 'Stoltcade' })).toBeVisible()
-        await expect(page.getByText('PeriodicPairs')).toBeVisible()
-        await expect(page.getByText('Welcome to Stoltcade!')).toBeVisible()
+        await expect(page.getByRole('link', { name: 'Stoltcade' })).toBeVisible({ timeout: 30000 })
+        await expect(page.getByText('PeriodicPairs')).toBeVisible({ timeout: 30000 })
+        await expect(page.getByText('Welcome to Stoltcade!')).toBeVisible({ timeout: 30000 })
     })
 
     test('game description is shown when hovering cursor over a game button', async ({ page }) => {
         const gameButton = page.getByRole('button', { name: 'PeriodicPairs' })
-        await expect(gameButton).toBeVisible()
+        await expect(gameButton).toBeVisible({ timeout: 30000 })
         await gameButton.hover()
 
         const description = page.getByText('A quiz game about the periodic table.')
-        await expect(description).toBeVisible({ timeout: 3000 })
+        await expect(description).toBeVisible({ timeout: 30000 })
     })
 
     test('game is opened when game button is clicked', async ({ page }) => {
         const gameButton = page.getByRole('button', { name: 'PeriodicPairs' })
-        await expect(gameButton).toBeVisible()
+        await expect(gameButton).toBeVisible({ timeout: 30000 })
         await gameButton.click()
 
         const how_to_play = page.getByText('Type the full name of the chemical element that matches the symbol shown on the screen and click Submit.')
-        await expect(how_to_play).toBeVisible({ timeout: 10000 })
+        await expect(how_to_play).toBeVisible({ timeout: 30000 })
     })
 
     test('navigate back to front page from play page after clicking Stoltcade header', async ({ page }) => {
         const gameButton = page.getByRole('button', { name: 'PeriodicPairs' })
-        await expect(gameButton).toBeVisible()
+        await expect(gameButton).toBeVisible({ timeout: 30000 })
         await gameButton.click()
 
         const how_to_play = page.getByText('Type the full name of the chemical element that matches the symbol shown on the screen and click Submit.')
-        await expect(how_to_play).toBeVisible({ timeout: 10000 })
+        await expect(how_to_play).toBeVisible({ timeout: 30000 })
 
         const header = page.getByRole('link', { name: 'Stoltcade' })
-        await expect(header).toBeVisible()
+        await expect(header).toBeVisible({ timeout: 30000 })
         await header.click()
 
         const welcome = page.getByText('Welcome to Stoltcade!')
-        await expect(welcome).toBeVisible({ timeout: 10000 })
+        await expect(welcome).toBeVisible({ timeout: 30000 })
     })
 })
