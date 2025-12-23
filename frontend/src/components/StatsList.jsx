a2b8d12f98dd55cac703214400e226a88c828c9f
westolt <william.stolt@helsinki.fi>
2025-12-23 23:15:56 +0200

    Refactor Stats.jsx and make it css better

--- frontend/src/components/StatsList.jsx
+++ frontend/src/components/StatsList.jsx
@@ -15,25 +15,41 @@ const StatsList = ({ scores }) => {
         const diffSec = Math.floor((currentTime - updatedAt) / 1000)
 
         if (diffSec < 60) return 'now'
-        if (diffSec < 3600) return `${Math.floor(diffSec / 60)} minutes ago`
-        if (diffSec < 86400) return `${Math.floor(diffSec / 3600)} hours ago`
-        return `${Math.floor(diffSec / 86400)} days ago`
+        if (diffSec < 3600) {
+            const result = Math.floor(diffSec / 60)
+            const unit = result === 1 ? 'minute' : 'minutes'
+            return `${result} ${unit} ago`
+        }
+        if (diffSec < 86400) {
+            const result = Math.floor(diffSec / 3600)
+            const unit = result === 1 ? 'hour' : 'hours'
+            return `${result} ${unit} ago`
+        } 
+        const result = Math.floor(diffSec / 86400)
+        const unit = result === 1 ? 'day' : 'dayss'
+        return `${result} ${unit} ago`
     }
 
     return(
-        <div>
-            <p>User  Game   Score  Time</p>
-            <div className='scores'>
-                {scores
-                .map(highscore =>{
-                    const updatedAt = new Date(highscore.updatedAt)
+        <div className='stats_table'>
+            <div className='score_row'>
+                <div className='score_cell'>User</div>
+                <div className='score_cell'>Game</div>
+                <div className='score_cell'>Score</div>
+                <div className='score_cell'>Time</div>
+            </div>
 
-                    return(
-                        <div key={highscore.id}>
-                            {highscore.user.username}: {highscore.game.name} {highscore.score}
-                            {' '}- {handleTime(updatedAt, currentTime)}
-                        </div>
-                    )
+            <div className='scores'>
+                {scores.map(highscore => {
+                const updatedAt = new Date(highscore.updatedAt)
+                return (
+                    <div className='score_row' key={highscore.id}>
+                    <div className='score_cell'>{highscore.user.username}</div>
+                    <div className='score_cell'>{highscore.game.name}</div>
+                    <div className='score_cell'>{highscore.score}</div>
+                    <div className='score_cell'>{handleTime(updatedAt, currentTime)}</div>
+                    </div>
+                )
                 })}
             </div>
         </div>
